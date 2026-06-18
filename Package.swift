4a68c2c2e6693fd16767b51bc3b598fe9e817bc3
johnschekin <john.schekin11@gmail.com>
2026-06-17 23:19:34 -0400

    bump macOS floor 10.15 -> 13 (auAudioUnit needs macOS 13; consumer app targets macOS 27)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "LiveKit",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
+        .macOS(.v13),
         .macCatalyst(.v14),
         .tvOS(.v17),
     ],
